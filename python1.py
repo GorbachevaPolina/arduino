34ebb19a25536d123cc0675b9ba19ad97e8b2abe
SvetlanaSarvartdinova <71510191+SvetlanaSarvartdinova@users.noreply.github.com>
2020-10-10 15:08:03 +0300

    update

--- python1.py
+++ python1.py
@@ -3,29 +3,19 @@ import json
 import websockets
 import socket
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-
-
-def func(dct):
-    return (int(dct['x']), int(dct['y']))
+IP = "192.168.1.72"
 
 
 async def connect(websocket, path):
     async for message in websocket:
-        print(message)
-        a = json.dumps(message)
-        data = json.loads(a, object_hook=func)
-        #data = json.loads('{"x":1,"y":2}', object_hook=func)
-        print(data[0])
-        # x = str(data[0]).encode()
-        # y = str(data[1]).encode()
-        # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # sock.sendto(x, (UDP_IP, UDP_PORT))
-        # sock.sendto(y, (UDP_IP, UDP_PORT))
+        data = json.loads(message)
+        x = str(data["x"]).encode()
+        y = str(data["y"]).encode()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(x, (IP, 5005))
+        sock.sendto(y, (IP, 5005))
 
 
-start_server = websockets.serve(connect, "192.168.43.188", 5679)
-
+start_server = websockets.serve(connect, IP, 5679)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
