6827af055c52e70a01b1d8b9cc1d32ef30e79dbd
SvetlanaSarvartdinova <71510191+SvetlanaSarvartdinova@users.noreply.github.com>
2020-10-21 14:25:44 +0300

    update sending

--- python1.py
+++ python1.py
@@ -3,19 +3,19 @@ import json
 import websockets
 import socket
 
-IP = "192.168.1.72"
+IP1 = "192.168.43.188"
+IP2 = "192.168.43.167"
 
 
 async def connect(websocket, path):
     async for message in websocket:
         data = json.loads(message)
-        x = str(data["x"]).encode()
-        y = str(data["y"]).encode()
+        mess = bytes([data["x"]+100, data["y"]+100])
+        print(data["x"], data["y"])
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(x, (IP, 5005))
-        sock.sendto(y, (IP, 5005))
+        sock.sendto(mess, (IP2, 5005))
 
 
-start_server = websockets.serve(connect, IP, 5679)
+start_server = websockets.serve(connect, IP1, 5679)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
